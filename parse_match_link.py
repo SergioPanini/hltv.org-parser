f77d69766df3c41cbb28e2da97b06d67ae38aaaf
SergeyPanin <sergey.panin.2019@gmail.com>
2021-09-01 16:05:46 +0300

    Парсинг всех данных матчей

--- parse_match_link.py
+++ parse_match_link.py
@@ -18,24 +18,44 @@ def main():
 
     result_matchs_count = 100
     for i in range(result_matchs_count // 100):
-        URL += f'?offset={i * 100}'
+        newURL = URL + f'?offset={i * 100}'
 
         
-        response = requests.get(URL, headers=headers)
-        print('URL: ', URL, 'status code: ', response.status_code)
+        response = requests.get(newURL, headers=headers)
+        print('URL: ', newURL, 'status code: ', response.status_code)
 
-        with open(f'files_{i}.html', 'w') as f:
+        with open(f'./files/files_{i}.html', 'w') as f:
             f.write(response.text)
     
 
         soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
-        all_results = soup.find_all(class_='results-all')[-1]
-        result_matches = all_results.find_all(class_='results-sublist')
-        
-        for result in result_matches:
-            data = result.find(class_='standard-headline')
-            print('Data: ', data.text)
+        #Находим блок в сезультатами
+        results_all = soup.find_all(class_='results-all')[-1]
+
+        #Находим даты, в которых были матчи
+        results_sublist = results_all.find_all(class_='results-sublist')
+
+        with open(f'./files/files_match{1}.html', 'w') as f:
+            f.write(str(results_sublist))
+            
+        #Находим матчи и саму дату
+        for sublist in results_sublist:
+            date = sublist.find(class_='standard-headline').text
+            matches = sublist.find_all(class_='result-con')
+
+            print('Date: ', date)
+            print('Count matches: ', len(matches))
+
+            #Находим названия матчей
+            for i, match in enumerate(matches):
+                teams = [team.text for team in match.find_all(class_='team')]
+                event_name = match.find(class_='event-name').text
+                result_score = match.find(class_='result-score').text
+                match_url = 'hltv.org' + match.a['href']
+
+                print(i, teams, result_score, event_name, match_url)
+
 
 if __name__ == '__main__':
     main()
